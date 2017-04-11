58637fed7f3d918490a62c97cf54692d3a5e2a0d
Philipp Meisberger <team@pm-codeworks.de>
2017-04-11 20:26:05 +0200

    Added encoding of characteristics in UTF-8 to avoid exception "Unicode-objects must be encoded before hashing" in Python3 hashlib

--- src/files/examples/example_search.py
+++ src/files/examples/example_search.py
@@ -62,7 +62,7 @@ try:
     f.loadTemplate(positionNumber, 0x01)
 
     ## Downloads the characteristics of template loaded in charbuffer 1
-    characterics = str(f.downloadCharacteristics(0x01))
+    characterics = str(f.downloadCharacteristics(0x01)).encode('utf-8')
 
     ## Hashes characteristics of template
     print('SHA-2 hash of template: ' + hashlib.sha256(characterics).hexdigest())
