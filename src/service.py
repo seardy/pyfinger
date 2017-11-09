c2d5d1e9a9cfd52b75bf5f14eb83b8fc61c4c6a0
seardy <sarteaga@protonmail.com>
2017-11-09 16:16:38 -0500

    client fixed

--- src/service.py
+++ src/service.py
@@ -3,8 +3,9 @@ from pymongo import MongoClient
 
 def marcar_asistencia(id):
     # Coleccion de datos de los empleados
-    mongo = MongoClient('mongodb://testing:test@ds155934.mlab.com:55934/testing_this_shit')
-    user = mongo.db.users
+    client = MongoClient('mongodb://testing:test@ds155934.mlab.com:55934/')
+    db = client['testing_this_shit']
+    user = db.users
     # Datos enviados en la peticion post para llenar la asistencia
     fecha = datetime.now()
     dia = fecha.weekday()
