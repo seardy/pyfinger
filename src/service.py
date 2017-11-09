f304714499ea99d7358acbb4a119214b362a2b0a
seardy <sarteaga@protonmail.com>
2017-11-09 16:59:28 -0500

    asd

--- src/service.py
+++ src/service.py
@@ -3,7 +3,11 @@ from pymongo import MongoClient
 
 def marcar_asistencia(id):
     # Coleccion de datos de los empleados
-    mongo = MongoClient("mongodb://testing:test@ds155934.mlab.com:55934/testing_this_shit?authMechanism=SCRAM-SHA-1")
+    mongo = MongoClient('ds155934.mlab.com:55934',
+                        user='testing',
+                        password='test',
+                        authSource='testing_this_shit',
+                        authMechanism='SCRAM-SHA-1')
     user = mongo.db.users
     # Datos enviados en la peticion post para llenar la asistencia
     fecha = datetime.now()
