302472b2a7878e1538dd7bef9900b6513553c516
seardy <sarteaga@protonmail.com>
2017-11-13 17:16:44 -0500

    identation finished

--- src/test.py
+++ src/test.py
@@ -7,16 +7,16 @@ search_thread = None
 
 
 def on_message(client, userdata, message):
+    if message.topic == "enroll/begin":
+        fp.abort = True
+        data = json.loads(message.payload)
+        global search_thread
+        search_thread.join()
+        fp.enroll(data['identificacion'])
+    if message.topic == "search/finished":
+        search_thread = threading.Thread(target=fp.search)
+        search_thread.start()
 
-	if message.topic == "enroll/begin":
-		fp.abort = True
-		data = json.loads(message.payload)
-		global search_thread
-		search_thread.join()
-		fp.enroll(data['identificacion'])
-	if message.topic == "search/finished":
-        	search_thread = threading.Thread(target=fp.search)
-		search_thread.start()
 
 fp = Fingerprint()
 client = paho.Client("routine")
