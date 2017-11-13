fee435e625cb012c9cc05bbfb377f7330e0eed2b
seardy <sarteaga@protonmail.com>
2017-11-13 17:33:31 -0500

    broken_pipe

--- src/fingerprint.py
+++ src/fingerprint.py
@@ -81,6 +81,9 @@ class Fingerprint:
                     print('Found template at position #' + str(position_number))
                     print('The accuracy score is: ' + str(accuracy_score))
                     return
+        except IOError, e:
+            print("Cachado hijueputa")
+            print(e.message)
         except Exception as e:
             print('Operation failed!')
             print('Exception message: ' + str(e))
